5fd45235bb29d79e8ac740757a77adae43ff28ac
Robert_BTTE <115649439+RABZ49@users.noreply.github.com>
2023-11-20 16:06:01 -0500

    Add files via upload

--- Proyecto.js
+++ Proyecto.js
@@ -1,41 +1,45 @@
-// Almacenar reservas en memoria
-let reservations = []; 
+// Reservas
+let reservations = [];
 
-// Comprobar disponibilidad
-function checkAvailability(datetime) {
-  // Buscar conflicos de horarios  
+// Comprobar disponibilidad 
+const checkAvailability = (datetime) => {
   return reservations.every(r => r.datetime !== datetime);
 }
 
 // Crear reserva
-function createReservation(name, datetime) {
-  if (checkAvailability(datetime)) {
-    const newReservation = {
-      name,
-      datetime
-    };
-    
+const createReservation = (name, datetime) => {
+  if(checkAvailability(datetime)) {
+    let newReservation = {name, datetime};
     reservations.push(newReservation);
-    
-    return true; // Reserva creada
+    return true;
   }
-  
-  return false; // Horario no disponible
+  return false;
+}  
+
+// Mostrar reservas
+const showReservations = () => {
+  let html = '<ul>';
+  reservations.forEach(r => {
+    html += `<li>${r.name} - ${r.datetime}</li>`;
+  });
+  html += '</ul>';
+  document.getElementById('reservations').innerHTML = html;
 }
 
-// Lógica del botón reserva
-const form = document.getElementById('reservation-form');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const name = form.elements['name'].value;
-  const datetime = form.elements['datetime'].value;
-
-  if (createReservation(name, datetime)) {
-    showConfirmation(name);
-  } else {
-    alert("Lo sentimos, ese horario no está disponible");
-  }
-
+// Handler para botón Reservar
+document.getElementById('reservation-form')
+  .addEventListener('submit', e => {
+    e.preventDefault();
+    let name = document.getElementById('name').value;
+    let datetime = document.getElementById('datetime').value;    
+    if(createReservation(name, datetime)){
+      alert('Reserva creada!');
+      showReservations();
+    } else {
+      alert('Horario no disponible');
+    }
 });
+
+// Handler para botón Ver Reservas
+document.getElementById('see-reservations')
+  .addEventListener('click', showReservations);
